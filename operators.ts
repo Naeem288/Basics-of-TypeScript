d3bbecd6cad7cb1332886c74f5ac244931eda7b5
Naeem288 <93144624+Naeem288@users.noreply.github.com>
2023-04-03 09:44:39 +0500

    Operators
    Here, We are going to discuss the different operators like arithmetic, logical, etc.

---
+++ operators.ts
@@ -0,0 +1,35 @@
+// Arithmatic Operator
+let n1 = 3 , n2 = 4;
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 ** n2) // to find the power
+console.log(n1 % n2) // to find the mod
+let str1 = "1", str2 = "2";
+console.log(str1 + str2)
+// Assignemt operator
+let n = 5;
+console.log(n);
+n++;
+console.log(n);
+n+=1;
+console.log(n);
+n-=1;
+console.log(n);
+n*=1;
+console.log(n);
+n/=1;
+console.log(n);
+// Comparision operators
+let s1= 5;
+let s2 = 3;
+// to compare the valuest
+console.log(n1 == n2);
+// to compare the values as well as type of values
+console.log(n1 === n2);
+// relational and logical operators
+console.log(n1 > 3 &&  n2 > 4);
+console.log(n1 > 3 ||  n2 > 4);
+
+
